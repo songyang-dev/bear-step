363fff2f0bc064a74b28eb6ed5ccf8b0d4656411
Song Yang <songyang1218@gmail.com>
2020-05-26 20:55:47 -0400

    Add orb message UI test

---
+++ Assets/Scripts/Tests/OrbUIResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class OrbUIResponse : LevelSceneSetUp
+    {
+        [UnityTest]
+        public IEnumerator OrbUIDisplayDuration()
+        {
+            yield return SetUp("Test/cross");
+
+            var ui = root.GetComponentInChildren<OrbMessageUI>();
+
+            ui.Display("Test 1", 1f);
+            yield return new WaitForSeconds(1f);
+            Assert.AreEqual("Test 1", ui.GetComponent<Text>().text, $"Text is not the same: {ui.GetComponent<Text>().text}");
+        }
+    }
+}
