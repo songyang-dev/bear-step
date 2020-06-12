e69fac24c0719e3947dd401e8e5e7b9c29fec06a
Song Yang <songyang1218@gmail.com>
2020-06-12 18:20:22 -0400

    Change to new input system's event system

--- Assets/Scripts/Tests/PausedBehaviours.cs
+++ Assets/Scripts/Tests/PausedBehaviours.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine.UI;
 using UnityEngine.TestTools;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
@@ -32,6 +32,32 @@ namespace Tests
 
             Assert.AreEqual(1, Time.timeScale, "Time not unfrozen");
             Assert.False(menuObject.activeInHierarchy, "Menu still present");
+        }
+
+        [UnityTest]
+        public IEnumerator BackToMainMenu()
+        {
+            yield return SetUp("Test/pool");
+            
+            var pauseMenu = root.GetComponentInChildren<PauseMenu>();
+
+            // get the ui panel that is disabled by default
+            var menuObject = pauseMenu.transform.GetChild(0).gameObject; // assuming that it is the only child
+
+            Assert.False(pauseMenu.GameIsPaused, "Game is paused initially");
+            
+            pauseMenu.Pause();
+            yield return null;
+
+            Assert.AreEqual(0, Time.timeScale, "Time not frozen");
+            Assert.True(menuObject.activeInHierarchy, "Menu does not appear");
+
+            var currentScene = SceneManager.GetActiveScene().name;
+
+            pauseMenu.Menu();
+
+            Assert.AreEqual(currentScene, SceneManager.GetActiveScene().name, "Scene did not change");
+
         }
     }
 }
